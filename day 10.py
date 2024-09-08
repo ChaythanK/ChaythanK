7937da00f3ac9b02f25f6a722960b8a6e5e0fdb2
Chaythan <ghaythank18@gmail.com>
2024-09-07 21:45:38 -0400

    Added a duplicate print just for testing git

--- day 10.py
+++ day 10.py
@@ -1,7 +1,8 @@
 import leap_year_calc
 from leap_year_calc import is_a_leap
 from leap_year_calc import leap_year
-print(leap_year_calc.leap_year.__doc__) 
+print(leap_year_calc.leap_year.__doc__)
+print(leap_year_calc.leap_year.__doc__)
 print("welcome to the days of the year calculator")
 r=int(input("what is the year?, "))
 a=leap_year(r)
